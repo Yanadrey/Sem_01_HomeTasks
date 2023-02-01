9381441bb55a3dabed978842fc3ee44959593f79
Yana <YanaDrey@rambler.ru>
2023-02-01 18:28:00 +0300

    Task_003

---
+++ Task_003/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+    System.Console.WriteLine($"{number} - четное число");
+else
+    System.Console.WriteLine($"{number} - нечетное число");
