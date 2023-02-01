11f0130b93a4c9d9371f52afb3a37538929b425d
Yana <YanaDrey@rambler.ru>
2023-02-01 18:47:22 +0300

    Task_004

---
+++ Task_004/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 2)
+    System.Console.WriteLine($"Нет положительных четных числел < {number}");
+else
+{
+    for (int i = 2; i <= number; i++)
+    {
+        if (i % 2 == 0)
+            Console.Write($"{i} ");
+    }
+}
